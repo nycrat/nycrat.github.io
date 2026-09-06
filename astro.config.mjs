acaa77af18b97b2cb306a6d17cea5f2951627bf5
Avah Xiao <avahxiao@gmail.com>
2026-09-06 15:38:03 -0700

    Add astro config options

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nycrat.dev/",
+  trailingSlash: "always",
   prefetch: {
     prefetchAll: true,
   },
